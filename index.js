f1e69c13f5576009a27ef6bee14a810d80212bb1
Optimex <100842932+EminentDCoder@users.noreply.github.com>
2022-10-21 08:48:11 +0000

    Add Maximum Stage

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ do {
   var num = (guessNumber(stage));
   var userGuess = prompt(`Guess a number betweeen 1 - ${stage}`);
   stage++;
+  if (stage > 10) break;
 }
-while (userGuess == num);
-console.log("Game Over");
+while (userGuess == num && stage <= 10);
+if (stage > 10) {
+  console.log("Wao! You Have Reached The Maximum Stage");
+}
+else {
+  console.log("Wrong Guesss, Game Over");
+}
+
